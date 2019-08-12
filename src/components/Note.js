ec0e8741c08a067238b174d5c58fe3f62a00e108
mibrahim <politokuet2k2@gmail.com>
2019-08-12 22:44:00 +0200

    make update

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
  const Note = ({note, toggleImportance}) => {
-     const level = note.important ? 'make not important' : 'make important';
+     const level = note.important ? 'make important' : 'make not important';
     return (
         <div>
             <li>
                 {note.content}
-                <button onClick = {() => toggleImportance}>{level}</button> 
+                <button onClick = {() => toggleImportance()}>{level}</button> 
             </li>
         </div>
     )
